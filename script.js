0591157285cbe54e322206e169b40cf6a8055371
SukaziN <nkosiyazisukazie@gmail.com>
2025-08-22 14:34:52 +0200

    Added Javascript for date and time live updates

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
+function updateTime() {
+  let pretoriaElement = document.querySelector("#pretoria");
+  let pretoriaDateElement = pretoriaElement.querySelector(".date");
+  pretoriaDateElement.innerHTML = moment()
+    .tz("Africa/Johannesburg")
+    .format("MMMM Do YYYY");
+  let pretoriaTimeElement = pretoriaElement.querySelector(".time");
+  let pretoriaTime = moment();
+  pretoriaTimeElement.innerHTML = pretoriaTime.format(
+    "[<strong>]hh:mm:ss[</strong>] [<small>]A[</small>]"
+  );
 
+  let sydneyElement = document.querySelector("#sydney");
+  let sydneyDateElement = document.querySelector("#sydney .date");
+  sydneyDateElement.innerHTML = moment()
+    .tz("Australia/Sydney")
+    .format("MMMM Do YYYY");
+  let sydneyTimeElement = sydneyElement.querySelector(".time");
+  let sydneyTime = moment().tz("Australia/Sydney");
+  sydneyTimeElement.innerHTML = sydneyTime.format(
+    "[<strong>]hh:mm:ss[</strong>] [<small>]A[</small>]"
+  );
+}
+updateTime();
+setInterval(updateTime, 1000);
